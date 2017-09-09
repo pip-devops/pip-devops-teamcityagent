f05a9494cfd96b48d5b6e45a17534d2362a9c325
Sergey Seroukhov <seroukhov@gmail.com>
2017-09-09 20:07:12 +0300

    Moved to teamcity 2017.1.4

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t pipdevs/teamcity-agent:2017.1.3 .
+docker build -t pipdevs/teamcity-agent:2017.1.4 .
--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker login
-docker push pipdevs/teamcity-agent:2017.1.3
+docker push pipdevs/teamcity-agent:2017.1.4
