3828e40e4e05ddbc588ab78ab960f6becbbbb5b2
Sergey Seroukhov <seroukhov@gmail.com>
2017-09-09 14:27:34 +0300

    Improved build and release scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
+
 docker build -t pipdevs/teamcity-agent:2017.1.3 .
-docker push pipdevs/teamcity-agent:2017.1.3
---
+++ release.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker login
+docker push pipdevs/teamcity-agent:2017.1.3
